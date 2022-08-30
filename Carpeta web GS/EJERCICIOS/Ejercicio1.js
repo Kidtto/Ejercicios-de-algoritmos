b54e76d6381ac3e36ed42737b5537c3d66435e9a
Kidtto <109235894+Kidtto@users.noreply.github.com>
2022-08-30 08:44:55 -0500

    Add files via upload

--- Carpeta web GS/EJERCICIOS/Ejercicio1.js
+++ Carpeta web GS/EJERCICIOS/Ejercicio1.js
@@ -1,12 +1,39 @@
 'use strict'
 // 1 Elaborar un programa que pida al usuario 2 numeros y se realicen las 4 operaciones basicas
  let num1
+   num1 = prompt("Ingrese un numero");
  let num2
-  num1 = prompt("Ingrese numero 1");
-  num2 = prompt("Ingrese numero 2");
+  num2 = prompt("Ingrese un numero");
+  let suma
+  suma =  parseFloat(num1)+ parseFloat(num2);{
+  console.log("La suma es: "+suma);
+  alert("La suma es "+ suma);
+}
 
-//Suma
-let suma
-suma =0;
- suma(p)
-  
+/*
+    function suma(num1,num2){ 
+   suma = num1 + num2;
+    console.log("La suma es"+suma);
+    alert("La suma es"+suma);
+ }
+  function resta(num1,num2){
+  resta = num1 - num2;
+   console.log("La resta es"+resta);
+   alert("La resta es"+resta);
+  }
+  function multiplicacion(num1,num2){
+    multiplicacion = num1 * num2;
+   console.log("La multiplicacion es");
+   alert("La multiplicacion es"+multiplicacion);
+  }
+  function division(num1,num2){
+   division = num1 / num2;
+    console.log("La division es"+divicion);
+    alert("La division es"+ division);
+  }
+   
+function resultado(suma,resta,multiplicacion,division){
+   resultado=suma,resta,multiplicacion,division;
+   console.log(resultado);
+}*/
+     
--- Carpeta web GS/EJERCICIOS/ejercicio2.js
+++ Carpeta web GS/EJERCICIOS/ejercicio2.js
@@ -1,13 +1,12 @@
 'use strict'
 //2 Leer un numero  e imprimir un mensaje si es positivo o negativo
 let num3
-num3 = prompt("Ingrese un numer");
+num3 = 88
 if (num3 > 0 ){
     console.log("Positivo");
 
 }else{
-    if(num3 < 0){
+
         console.log("Negativo");
     }
      
-}
