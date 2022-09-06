4f8a34dc385291032c0137fd154cd406a598f433
Kidtto <109235894+Kidtto@users.noreply.github.com>
2022-09-05 19:07:40 -0500

    Add files via upload

---
+++ Ejercicios/Ejercicio 5/main.js
@@ -0,0 +1,30 @@
+'use strict'
+let nota1 
+
+nota1 = prompt("Ingrese su nota de 1 a 5  ");
+let nota2 
+nota2 = prompt("Ingrese su nota de 1 a 5  ");
+let nota3 
+
+nota3 = prompt("Ingrese su nota de 1 a 5  ");
+
+
+/**nota1 = prompt("Ingrese su nota de 1 a 5  ");
+let nota2
+nota2 = prompt("Ingrese su nota de 1 a 5  ");
+let nota3
+nota3 = prompt("Ingrese su nota de 1 a 5 " );*/
+
+alert("Su primera nota fue: "+nota1);
+
+alert("Su segunda  nota fue: "+nota2);
+
+alert("Su Tercera nota fue: "+nota3);
+
+function promedio(nota1, nota2, nota3){
+    let pro = num1 + num2 + nota3 / 4;
+    alert("Su promedio es: "+pro);
+}
+
+
+
---
+++ Ejercicios/Ejercicio 6/main.js
@@ -0,0 +1,3 @@
+'use strict';
+let num
+num = prompt("Ingresar numero");
